c12ab44f127214aa640033a9aa3ac60cfcab72c8
Danilo <daniloaugusto0212@hotmail.com>
2021-02-22 22:30:57 -0300

    Criando outras seções

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap" rel="stylesheet">
 
     <title>Consultório Dentista</title>
 </head>
@@ -31,5 +31,92 @@
         </div>
         <!-- /.container -->
     </header>
+
+    <section class="mosaico">
+        <div class="container">
+            <div class="mosaico-wraper">
+                <div class="mosaico-single">
+                    <div class="img-mosaico-single"></div>
+                    <!-- /.img-mosaico-single -->
+
+                    <div class="img-mosaico-single"></div>
+                    <!-- /.img-mosaico-single -->
+                </div>
+                <!-- /.mosaico-single -->
+
+                <div class="mosaico-single">
+                    <div class="img-mosaico-single"></div>
+                    <!-- /.img-mosaico-single -->
+                    
+                    <div class="img-mosaico-single"></div>
+                    <!-- /.img-mosaico-single -->
+                </div>
+                <!-- /.mosaico-single -->
+
+                <div class="mosaico-single">
+                    <div class="img-mosaico-single"></div>
+                    <!-- /.img-mosaico-single -->
+                    
+                    <div class="img-mosaico-single"></div>
+                    <!-- /.img-mosaico-single -->
+                </div>
+                <!-- /.mosaico-single -->
+
+                <div class="mosaico-single">
+                    <div class="img-mosaico-single"></div>
+                    <!-- /.img-mosaico-single -->
+                    
+                    <div class="img-mosaico-single"></div>
+                    <!-- /.img-mosaico-single -->
+                </div>
+                <!-- /.mosaico-single -->
+
+                <div class="mosaico-single">
+                    <div class="img-mosaico-single"></div>
+                    <!-- /.img-mosaico-single -->
+                    
+                    <div class="img-mosaico-single"></div>
+                    <!-- /.img-mosaico-single -->
+                </div>
+                <!-- /.mosaico-single -->
+
+                <div class="mosaico-single">
+                    <div class="img-mosaico-single"></div>
+                    <!-- /.img-mosaico-single -->
+                    
+                    <div class="img-mosaico-single"></div>
+                    <!-- /.img-mosaico-single -->
+                </div>
+                <!-- /.mosaico-single -->
+            </div>
+            <!-- /.mosaico-wraper -->
+        </div>
+        <!-- /.container -->
+    </section>
+    <!-- /.mosaico -->
+
+    <section class="servicos">
+        <div class="container">
+            <div class="box-servico">
+                <h2>Lorem Ipsum</h2>
+                <p>Do cupidatat veniam ea anim deserunt duis do labore in in eu sunt.</p>
+            </div>
+            <!-- /.box-servico -->
+
+            <div class="box-servico">
+                <h2>Lorem Ipsum</h2>
+                <p>Do cupidatat veniam ea anim deserunt duis do labore in in eu sunt.</p>
+            </div>
+            <!-- /.box-servico -->
+
+            <div class="box-servico">
+                <h2>Lorem Ipsum</h2>
+                <p>Do cupidatat veniam ea anim deserunt duis do labore in in eu sunt.</p>
+            </div>
+            <!-- /.box-servico -->
+        </div>
+        <!-- /.container -->
+    </section>
+    <!-- /.servicos -->
 </body>
 </html>
